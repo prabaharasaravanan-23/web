dd3c2ffa1c715fe1bc706f2752c1e42f03d6ef91
prabaharan <prabaharasaravanan@gmail.com>
2026-01-28 14:27:10 +0530

    new commit

---
+++ decisionmaking/dm.js
@@ -0,0 +1,65 @@
+/**let amount=5000
+
+if(amount>10000){
+    console.log("we can go to goa");
+}
+else if(amount>4000){
+    console.log("we can go to ooty");
+    
+}
+else if(amount>1000){
+    console.log("we can go to theme park");
+    
+}
+else if(amount<100){
+    console.log("we can go to tea kudika");
+    
+}**/
+
+
+//switch statement
+
+/**let day="monday"
+
+switch(day){
+    case "sunday":
+        console.log("biryani");
+    case "monday":
+        console.log("chicken");
+        break
+    case "tuesday":
+        console.log("fried rice");
+        break
+    case "wednesday":
+        console.log("curd rice");
+        break
+    case "thursday":
+        console.log("sambar rice");
+        break
+    case "friday":
+        console.log("lemon rice");
+        break
+    case "saturday":
+        console.log("parotta");
+        break
+        
+}**/
+
+//ternary operator
+
+let x=500
+let res=x%2==0 ? "even" : "odd"
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+    
+
+
