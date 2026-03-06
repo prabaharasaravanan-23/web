12b56853bc9fd3f88aef4f68368288418ab4f553
prabaharan <prabaharasaravanan@gmail.com>
2026-03-06 13:07:47 +0530

    saved

---
+++ json/copy.js
@@ -0,0 +1,57 @@
+// let obj={
+//     name:"vimal",
+//     age:23,
+//     native:"tnj",
+// }
+
+// let obj2=obj
+
+
+
+// obj2.name="naveen"
+// console.log(obj );
+
+// console.log(obj2);
+
+
+// let obj3={...obj}
+
+
+// obj3.age=22
+
+// console.log(obj3);
+// console.log(obj);
+
+
+
+// let obj4=Object.assign({},obj)
+
+// obj4.native="thanjavur"
+
+// console.log(obj);
+// console.log(obj4);
+
+
+let obj1={
+    name:"msd",
+    team:"india",
+    role:"captain",
+    retired:true,
+    local:{
+        teamname:"csk",
+        role:"wk",
+        teamretired:false,
+    }
+}
+
+let obj2={
+    name:"rohit",
+    team:"india",
+    role:"captain",
+    retired:true,
+    local:{
+        teamname:"mi",
+        role:"batman",
+        teamretired:false,
+    }
+}
